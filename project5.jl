2b857a8a9354090b2153cc3c78ff516a76370639
DmitryBal <76563919+DmitryBal@users.noreply.github.com>
2021-06-27 22:52:12 +0300

    Add files via upload

--- project5.jl
+++ project5.jl
@@ -174,12 +174,12 @@ end
 #вычисляющий наибольшее значение обобщенной частичной суммы числовой последовательности.
 
 function sub_sum(series)
-    elem = series[1]
+    el = series[1]
 	sum = 0
 	max_sum = 0
 for i in 1:length(series)
 	sum += series[i]
-	elem = min(elem, sum - max_sum)
+	el = min(el, sum - max_sum)
 	max_sum = max(max_sum, sum)
 end
 return max_sum
